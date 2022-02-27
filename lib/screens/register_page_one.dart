d734c31230437f03e0b6a7a9b038a65ae49651e3
uemirhanselim <uemirhanselim@gmail.com>
2022-02-27 13:19:52 +0300

    unused codes removed

--- lib/screens/register_page_one.dart
+++ lib/screens/register_page_one.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:test_pro/authentication/terms_of_use.dart';
